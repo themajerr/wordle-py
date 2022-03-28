32628d8f200a01610bf4eaddfe45daead8952ee4
Igor <igor.majerczak@student.uj.edu.pl>
2022-03-29 00:30:57 +0200

    zmiana lp na guess_counter, początek sprawdzania słowa

--- gui.py
+++ gui.py
@@ -1,9 +1,13 @@
 from tkinter import *
 from tkinter import messagebox
+import random
 
 
 def split(word):
     return[char for char in word]
+
+def get_a_word_from_dictionary():
+    correct_anwser = random.choice(word_dictionary)
 
 def entry_to_word():
     if guess_entry.get() not in word_dictionary:
@@ -13,33 +17,46 @@ def entry_to_word():
 
     word_guess.set(guess_entry.get())
     temp = word_guess.get()
-    word_list = split(temp)
+    global word_guess_listform
+    word_guess_listform = split(temp)
 
     #OSTATECZNIE TO NIE TUTAJ | ZOSTAWIAM DO DEBUGOWANIA
     #print(word_list)
     #guess_print = Label(root, text=word_list)
-    #guess_print.grid(column=0, row=lp.get())
+    #guess_print.grid(column=0, row=guess_counter.get())
 
-    lp_change()
-    update_status_indicator()
-
-def lp_change():
-    lp.set(lp.get()+1)
-    if lp.get() == 6:
+    #guess_counter_change()
+    #update_status_indicator()
+def guess_check():
+    temp = 0
+    for char in word_guess_listform:
+        if char == word_guess_listform[temp]:
+            temp+=1
+        if char in word_guess_listform:
+            temp+=1
+        if char not in word_guess_listform:
+            temp+=1
+    
+    guess_counter_change
+    
+def guess_counter_change():
+    guess_counter.set(guess_counter.get()+1)
+    if guess_counter.get() == 6:
         gameover()
 
 def gameover():
     return
 
 def update_status_indicator():
-    status_indicator = Label(root, text='Próba ' + str(lp.get()) + ' z 6  ', bd=1, relief=SUNKEN, anchor=E)
+    status_indicator = Label(root, text='Próba ' + str(guess_counter.get()) + ' z 6  ', bd=1, relief=SUNKEN, anchor=E)
     status_indicator.grid(column=0, row=8, columnspan=6, sticky=W+E, padx=5, pady=5)
 root = Tk()
 root.title('Wordle: Igor Edition')
 root.iconbitmap('a.ico')
+
 word_guess = StringVar(root)
 word_list = []
-lp = IntVar(root, 1)
+guess_counter = IntVar(root, 1)
 
 #cały słownik, zmienne i zawartość
 word_dictionary_txt_temp = open('lista.txt').read().split('\n')
