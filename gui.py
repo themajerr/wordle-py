581d52d053a7cfb777df6ca8c134f7da8c454006
Igor <igor.majerczak@student.uj.edu.pl>
2022-03-28 23:56:11 +0200

    Pasek się już nawer aktualizuje xd

--- gui.py
+++ gui.py
@@ -9,7 +9,8 @@ def entry_to_word():
     if guess_entry.get() not in word_dictionary:
         messagebox.showerror('Błąd!', 'Słowo nie znajduje się w słowniku!')
         guess_entry.delete(0, END)
-        
+         
+
     word_guess.set(guess_entry.get())
     temp = word_guess.get()
     word_list = split(temp)
@@ -20,11 +21,19 @@ def entry_to_word():
     #guess_print.grid(column=0, row=lp.get())
 
     lp_change()
-    
+    update_status_indicator()
 
 def lp_change():
     lp.set(lp.get()+1)
+    if lp.get() == 6:
+        gameover()
 
+def gameover():
+    return
+
+def update_status_indicator():
+    status_indicator = Label(root, text='Próba ' + str(lp.get()) + ' z 6  ', bd=1, relief=SUNKEN, anchor=E)
+    status_indicator.grid(column=0, row=8, columnspan=6, sticky=W+E, padx=5, pady=5)
 root = Tk()
 root.title('Wordle: Igor Edition')
 root.iconbitmap('a.ico')
