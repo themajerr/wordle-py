6b1f30b9c3f05243fccf16b46030feb9435e52de
Igor <igor.majerczak@student.uj.edu.pl>
2022-04-04 00:22:28 +0200

    win and gameover conditions checked on GUI part - early version to be improved later

--- gamesystem.py
+++ gamesystem.py
@@ -1,16 +1,17 @@
 import random
-from enum import Enum
+from enum import Enum, auto
 
 
 class Wordle:
     def split(word):
         return[char for char in word]
 
-    guess_counter = 0
-    max_number_of_guesses = 6
+    #guess_counter = 0
+    #max_number_of_guesses = 6
     expected_word_length = 5
     is_win = False
     is_gameover = False
+    
     # Dictionary creation
     word_dictionary_txt_temp = open('lista.txt').read().split('\n')
     word_dictionary = list()
@@ -25,42 +26,42 @@ class Wordle:
     correct_answer_list = split(correct_answer)
     print(correct_answer)
     
+    # Enum creation
+    class Result(Enum):
+        CORRECT_WORD_CORRECT_PLACE = auto()
+        CORRECT_WORD_INCORRECT_PLACE = auto()
+        INCORRECT_WORD_INCORRECT_PLACE = auto()
+
     def __new__(self, passed_word):
         # setup for new word
         entered_word = self.split(passed_word)
         char_position = 0
         guess_check_results = []
-
-        # Enum creation
-        class Result(Enum):
-            CORRECT_WORD_CORRECT_PLACE = 1
-            CORRECT_WORD_INCORRECT_PLACE = 2
-            INCORRECT_WORD_INCORRECT_PLACE = 3
-
+        
         # validation mechanism
                  
         for char in entered_word:
             if char == self.correct_answer_list[char_position]:
-                guess_check_results.append(Result.CORRECT_WORD_CORRECT_PLACE.value)
+                guess_check_results.append(self.Result.CORRECT_WORD_CORRECT_PLACE.value)
                         
             elif char in self.correct_answer_list:
-                guess_check_results.append(Result.CORRECT_WORD_INCORRECT_PLACE.value)
+                guess_check_results.append(self.Result.CORRECT_WORD_INCORRECT_PLACE.value)
                         
             else:    
-                guess_check_results.append(Result.INCORRECT_WORD_INCORRECT_PLACE.value)                 
+                guess_check_results.append(self.Result.INCORRECT_WORD_INCORRECT_PLACE.value)                 
                         
             char_position+=1
 
-        self.guess_counter+=1   
+        #self.guess_counter+=1   
         
-        if entered_word == self.correct_answer_list:
-            self.is_win = True 
+        #if entered_word == self.correct_answer_list:
+        #    self.is_win = True 
 
         print(guess_check_results)
         result = guess_check_results
 
-        if self.guess_counter == self.max_number_of_guesses:
-            self.is_gameover = True
+        #if self.guess_counter == self.max_number_of_guesses:
+        #    self.is_gameover = True
             
         
         return result
--- gui.py
+++ gui.py
@@ -1,11 +1,16 @@
+from threading import get_ident
 from tkinter import *
 from tkinter import messagebox
 import gamesystem
 
+global guess_counter
+guess_counter = 0
+
 def whole_guess_operation():
 
     #entry to word + dictionary check -> pass word to gamesystem
-    
+    global guess_counter
+
     entered_word = guess_entry.get()
     entered_word_letters = entered_word
     if entered_word not in gamesystem.Wordle.word_dictionary:
@@ -23,30 +28,39 @@ def whole_guess_operation():
     for char in entered_word:
        
         temporary_color = ''
-        if entered_word[temporary_position] == 1:
+        if entered_word[temporary_position] == gamesystem.Wordle.Result.CORRECT_WORD_CORRECT_PLACE.value:
             temporary_color = '#569c38' #green
-        elif entered_word[temporary_position] == 2:
+        elif entered_word[temporary_position] == gamesystem.Wordle.Result.CORRECT_WORD_INCORRECT_PLACE.value:
             temporary_color = '#d6c527' #yellow
-        elif entered_word[temporary_position] == 3: 
+        elif entered_word[temporary_position] == gamesystem.Wordle.Result.INCORRECT_WORD_INCORRECT_PLACE.value: 
             temporary_color = '#7d796b' #grey
         
         print(temporary_color)
         colorful_box = Label(root, text=entered_word_letters[temporary_position], bg=temporary_color, fg='white', width=10, height=5, font=('Helvetica', 10, 'bold'))
-        colorful_box.grid(row=gamesystem.Wordle.guess_counter, column=temporary_position)
-        temporary_position+=1
-
-    #update guess indicator
+        colorful_box.grid(row=guess_counter, column=temporary_position)
         
-    status_indicator = Label(root, text='Guess ' + str(gamesystem.Wordle.guess_counter) + ' of 6  ', bd=1, relief=SUNKEN, anchor=E)
+        temporary_position+=1
+        
+    
+    #update guess indicator
+    
+    guess_counter+=1
+    status_indicator = Label(root, text='Guess ' + str(guess_counter) + ' of 6  ', bd=1, relief=SUNKEN, anchor=E)
     status_indicator.grid(column=0, row=8, columnspan=6, sticky=W + E, padx=5, pady=5)
 
     # check if the word was correct
-    if gamesystem.Wordle.is_win:
+    # komentarz żebym nie zapomniał, dlatego po januszowemu; jeżeli program ma otrzymać wyłącznie info [dobrze, dobrze, średnio] itd to on technicznie nie zna odpowiedzi więc to jest mocno na trytytce xd
+    if entered_word == [gamesystem.Wordle.Result.CORRECT_WORD_CORRECT_PLACE.value, 
+                        gamesystem.Wordle.Result.CORRECT_WORD_CORRECT_PLACE.value, 
+                        gamesystem.Wordle.Result.CORRECT_WORD_CORRECT_PLACE.value, 
+                        gamesystem.Wordle.Result.CORRECT_WORD_CORRECT_PLACE.value, 
+                        gamesystem.Wordle.Result.CORRECT_WORD_CORRECT_PLACE.value]:
         messagebox.showinfo(title="You won!", message='Congratulation! You have guessed the answer!')
         root.quit()
 
     # check for gameover
-    if gamesystem.Wordle.is_gameover:
+    # also komentarz - tutaj też teoretycznie nie zna poprawnej odpowiedzi xd
+    if guess_counter == max_number_of_guesses:
         messagebox.showerror(title='Game over!', message='Too bad! You have ran out of guesses!.\nCorrect answer is: ' + str(gamesystem.Wordle.correct_answer))
         root.quit()
 
@@ -55,6 +69,11 @@ root = Tk()
 root.title('Wordle: Igor Edition')
 root.iconbitmap('a.ico')
 
+# variables
+#guess_counter = 0
+max_number_of_guesses = 6
+
+# interface
 guess_entry = Entry(root, width=52, border=5, relief=SUNKEN)
 guess_entry.grid(column=0, row=7, columnspan=4, padx=5, pady=5)
 
